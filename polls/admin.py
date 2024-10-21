1443cd5667f277d103b58ccf34aed41789348361
regis066 <mugisharegis72@gmail.com>
2024-10-21 13:56:46 +0200

    Add Choice model into Question model using Tabular format

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
-from polls.models import Question
+from polls.models import Question, Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ["pub_date", "question_text"]
+    fieldsets = [
+        (None, {
+            'fields': [
+                'question_text',
+            ],
+        }),
+        (
+            'Date information',
+            {'fields': [
+                'pub_date'
+            ]}
+        )
+    ]
+
+    inlines = [ChoiceInLine]
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
